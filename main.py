8680c1bef9afd8a1696a5e9fb0743069901c68ff
Brendan Tang <brendantang616@gmail.com>
2026-09-01 15:54:10 -0400

    added face landmark detection

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
 FaceLandmarkerResult = mp.tasks.vision.FaceLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+latest_result = None
+
 # Create a face landmarker instance with the live stream mode:
 def print_result(result: FaceLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     print('face landmarker result: {}'.format(result))
+    global latest_result
+    latest_result = result
 
 options = FaceLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
+    num_faces=1,
     result_callback=print_result)
 
 with FaceLandmarker.create_from_options(options) as landmarker:
@@ -41,6 +46,16 @@ with FaceLandmarker.create_from_options(options) as landmarker:
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
         timestamp_ms = int((time.time()-start_time)*1000)
         landmarker.detect_async(mp_image, timestamp_ms)
+
+        if latest_result and latest_result.face_landmarks:
+            h, w = frame.shape[:2]
+            face_landmarks = latest_result.face_landmarks[0]
+
+            for lm in face_landmarks:
+                x = int(lm.x * w)
+                y = int(lm.y * h)
+
+                cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
         cv2.imshow("Webcam", frame)
         if cv2.waitKey(1) == ord('q'):
             break
