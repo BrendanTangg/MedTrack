7ec815ed6a6939f46b87e519e42301203486c705
Brendan Tang <brendantang616@gmail.com>
2026-09-01 14:50:21 -0400

    create face detection component

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import time
 
-stream = cv2.VideoCapture(0)
+model_path = "models/face_landmarker.task"
 
-if not stream.isOpened():
-    print("Stream failed :(")
-    exit()
+BaseOptions = mp.tasks.BaseOptions
+FaceLandmarker = mp.tasks.vision.FaceLandmarker
+FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+FaceLandmarkerResult = mp.tasks.vision.FaceLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-while(True):
-    ret, frame = stream.read()
-    if not ret:
-        print("No more stream :(")
-        break
+# Create a face landmarker instance with the live stream mode:
+def print_result(result: FaceLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('face landmarker result: {}'.format(result))
 
-    cv2.imshow("Webcam", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+options = FaceLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
 
-stream.release()
+with FaceLandmarker.create_from_options(options) as landmarker:
+  # The landmarker is initialized. Use it here.
+    stream = cv2.VideoCapture(0)
+
+    if not stream.isOpened():
+        print("Stream failed :(")
+        exit()
+
+    start_time = time.time()
+
+    while(True):
+        ret, frame = stream.read()
+        if not ret:
+            print("No more stream :(")
+            break
+
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        timestamp_ms = int((time.time()-start_time)*1000)
+        landmarker.detect_async(mp_image, timestamp_ms)
+        cv2.imshow("Webcam", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    stream.release()
 cv2.destroyAllWindows()
