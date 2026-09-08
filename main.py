e365696c49d33e9fa1ce196e083be2bfb6ad1be1
Brendan Tang <brendantang616@gmail.com>
2026-09-08 18:43:20 -0500

    added pill detection feature just using color & fixed timestamp problem

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import time
+import numpy as np
 
 model_path = "models/face_landmarker.task"
 
@@ -34,7 +35,8 @@ with FaceLandmarker.create_from_options(options) as landmarker:
         print("Stream failed :(")
         exit()
 
-    start_time = time.time()
+    start_time = time.monotonic()
+    last_timestamp_ms = -1
 
     while(True):
         ret, frame = stream.read()
@@ -42,9 +44,54 @@ with FaceLandmarker.create_from_options(options) as landmarker:
             print("No more stream :(")
             break
 
+        # Convert the frame from BGR to HSV
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Blue color range
+        lower_blue = np.array([100, 100, 100])
+        upper_blue = np.array([130, 255, 255])
+
+        # lower_red2 = np.array([170, 100, 100])
+        # upper_red2 = np.array([180, 255, 255])
+
+        # Create two masks because red wraps around the HSV color scale
+        mask = cv2.inRange(hsv_frame, lower_blue, upper_blue)
+        # mask2 = cv2.inRange(hsv_frame, lower_red2, upper_red2)
+
+        pill_mask = mask
+
+        # Find red objects
+        contours, _ = cv2.findContours(pill_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        pill_center = None
+
+        for contour in contours:
+
+            area = cv2.contourArea(contour)
+
+            # Ignore tiny red objects/noise
+            if area > 100:
+
+                x, y, pill_w, pill_h = cv2.boundingRect(contour)
+
+                pill_x = x + pill_w // 2
+                pill_y = y + pill_h // 2
+
+                pill_center = (pill_x, pill_y)
+
+                cv2.rectangle(frame, (x, y), (x+pill_w, y+pill_h), (0, 255, 255), 2)
+                cv2.circle(frame, pill_center, 3, (0, 255, 255), -1)
+                cv2.putText(frame, "Pill", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
-        timestamp_ms = int((time.time()-start_time)*1000)
+        timestamp_ms = int((time.monotonic()-start_time)*1000)
+
+        if timestamp_ms <= last_timestamp_ms:
+            timestamp_ms = last_timestamp_ms + 1
+
+        last_timestamp_ms = timestamp_ms
+
         landmarker.detect_async(mp_image, timestamp_ms)
 
         if latest_result and latest_result.face_landmarks:
